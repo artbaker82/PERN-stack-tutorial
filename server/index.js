3532a8f60e539171426c38772c1e680ed966b133
Arthur Baker <artbaker82@gmail.com>
2022-04-01 19:33:03 -0700

    db connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
+const pool = require('./db');
 //middleware
 app.use(cors())
 //allows us to get json from request.body
 app.use(express.json())
+
+//routes
+
+//create a todo
+
+app.post('/todos', async (req,res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1)',
+      [description]
+    )
+    res.json(newTodo)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+//get all
+
+//get a todo
+
+//update
+
+//deleta
+
+
+
+
 
 
 
